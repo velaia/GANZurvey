b60a2ddee1cca85e12cbd0fdd8f20bc0ba328322
Daniel N. Lang <daniel.lang@velaia.de>
2024-09-06 20:58:15 +0200

    - more context in README - cleaning up app.py a little

--- app.py
+++ app.py
@@ -21,9 +21,8 @@ NUM_IMAGES = len(images)
 @app.route('/', methods=[GET, POST])
 def display_survey():
     form = OneQuestionForm()
-    print('here')
     if form.validate_on_submit():
-        flash(f'Survey has been submitted.')
+        flash('Survey has been submitted.')
         save_result(form.questions.data)
         return redirect(url_for('thank_you'))
     session['permutations'] = form.imgs
@@ -33,9 +32,8 @@ def display_survey():
 @app.route('/gt', methods=[GET, POST])
 def display_survey_gt():
     form = OneQuestionForm()
-    print('here')
     if form.validate_on_submit():
-        flash(f'Survey has been submitted.')
+        flash('Survey has been submitted.')
         save_result(form.questions.data)
         return redirect(url_for('thank_you'))
     session['permutations'] = form.imgs
@@ -69,11 +67,10 @@ def save_result(data: list, ground_truth: bool = False):
     for i in range(len(data)):
         if data[i] in ('1', '2'):
             res = 0
-            print(f'data[i]: {data[i]}, perms[i][1][0]: {perms[i][1][0]}, file: {perms[i][3]}')
             if ((data[i] == '1' and perms[i][1][0] == 'A') or (data[i] == '2' and perms[i][1][0] == 'B')):
                 res = 1
             persist[perms[i][3]] = res
-    print(persist)
+    
     filename = f'results/result-{date.today()}-{datetime.now().time()}.pkl' if not ground_truth else f'results_gt/result-{date.today()}-{datetime.now().time()}.pkl'
     with  open(filename.replace(':', '_'), 'wb') as file:
         pickle.dump(persist, file)
